4d9aa9bbe8cac02ba6eaff719b57bf00a6e1e392
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2022-04-12 22:58:05 +1200

    Completed read_binary

--- src/chd.c
+++ src/chd.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "chd.h"
+#define LINE_SIZE 16
 
 int handle_read_file(FILE **fp, char *path) 
 {
@@ -13,12 +15,44 @@ int handle_read_file(FILE **fp, char *path)
     }
 }
 
+void print_byte_ascii(unsigned char line[], int length)
+{
+    for (int i = 0; i < length; i++) {
+        unsigned char temp_char = line[i];
+        if (isprint(temp_char)){
+            printf("%c", line[i]);
+        }
+        else {
+            putchar('.');
+        }
+    }
+}
+
 void read_binary(FILE *fp)
 {
     unsigned char buffer_byte;
+    int i = 0;
+    unsigned char line[LINE_SIZE];
     while (fread(&buffer_byte, sizeof(char), 1, fp) > 0) {
-        printf("%X", buffer_byte);
+        line[i] = buffer_byte;
+        printf("%02X ", buffer_byte);
+        if (i == LINE_SIZE - 1) {
+            printf(" | ");
+            print_byte_ascii(line, i+1);
+            putchar('\n');
+            i = 0;
+        } 
+        else {
+            ++i;
+        }
     }
+    // This part is just to print the remaining line in ascii
+    while (i < LINE_SIZE) {
+        printf("   ");
+        ++i;
+    }
+    printf(" | ");
+    print_byte_ascii(line, i+1);
 }
 
 
--- src/chd.h
+++ src/chd.h
@@ -2,5 +2,6 @@
 #define CHD_H
 int handle_read_file(FILE **fp, char *path);
 void read_binary(FILE *fp);
+void print_byte_ascii(unsigned char line[], int length);
 
 #endif
